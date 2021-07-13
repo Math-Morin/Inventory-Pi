60a605d56d7cebfc75ab2f72a9716313f0fa84ca
Mathieu <math_morin@msn.com>
2021-07-13 12:17:17 -0400

    Refactoring of the Multiple-in proto-logic with ORM

--- src/main.py
+++ src/main.py
@@ -1,64 +1,99 @@
-import sqlite3
+from sqlalchemy import create_engine
+from sqlalchemy import Column
+from sqlalchemy import Integer, String
+from sqlalchemy import insert, select
+from sqlalchemy.orm import Session, declarative_base
 
-connection = sqlite3.connect("test.db")
-cursor = connection.cursor()
 
-cursor.execute('''CREATE TABLE IF NOT EXISTS inventory
-                    (bar_code TEXT PRIMARY KEY NOT NULL,
-                    product_name TEXT NOT NULL,
-                    qty INT NOT NULL)''')
+Base = declarative_base()
+engine = create_engine(
+    'sqlite+pysqlite:///inventory.db',
+    echo=False,
+    future=True
+)
+
+
+class Inventory(Base):
+    __tablename__ = 'inventory'
+
+    barcode = Column(String, primary_key=True)
+    product = Column(String, nullable=False)
+    quantity = Column(Integer, nullable=False)
+
+    def __repr__(self):
+        return f"Inventory(bar_code={self.barcode!r}, product={self.product!r}, quantity={self.quantity!r})"
+
+
+Base.metadata.create_all(engine)
+
 
 print("Welcome to Inventory-Pi")
 
-running = True
-while running:
+while True:
     task_code = input("Please scan a task code... ")
 
     if task_code == "multiple-in":
-        print("'Multiple in' selected.")
+        print("'multiple-in' selected.")
 
         while True:
-            input_code_tuple = input("Please scan a product or the 'done' code... "),  # tuple
-            if input_code_tuple[0] == "done":
+            input_barcode = input(
+                "Please scan a product or the 'done' barcode... "
+            )
+
+            if input_barcode == "done":
                 break
 
-            db = list(cursor.execute("SELECT bar_code FROM inventory WHERE bar_code = ?", input_code_tuple))
+            with Session(engine) as session:
+                barcodes_in_inventory = session.execute(
+                    select(Inventory.barcode)
+                ).scalars()
 
-            if input_code_tuple in db:
-                cursor.execute("UPDATE inventory SET qty = qty + 1 WHERE bar_code = ?", input_code_tuple)
-                item = list(cursor.execute("SELECT * FROM inventory WHERE bar_code = ?", input_code_tuple))
-                print(f"{item[0][0]} {item[0][1]} quantity is now {item[0][2]}")
-            else:
-                input_name = input("Please enter the name of the product... ").lower().strip().replace(' ', '_'),
-                inputs = (input_code_tuple[0], input_name[0])
-                cursor.execute("INSERT INTO inventory VALUES (?, ?, 1)", inputs)
-                print(f"New item {inputs[0]} {inputs[1]} added to inventory with quantity 1")
+                if input_barcode in barcodes_in_inventory:
+                    product_to_update = session.execute(
+                        select(Inventory).
+                        filter_by(barcode=input_barcode)
+                    ).scalar_one()
+                    product_to_update.quantity += 1
 
-            connection.commit()
 
-    elif task_code == "multiple-out":
-        print("'Multiple out' selected.")
-
-        while True:
-            input_code_tuple = input("Please scan a product or the 'done' code... "),
-            if input_code_tuple[0] == "done":
-                break
-
-            db = list(cursor.execute("SELECT bar_code FROM inventory WHERE bar_code = ?", input_code_tuple))
-
-            if input_code_tuple in db:
-                qty = list(cursor.execute("SELECT qty FROM inventory WHERE bar_code = ?", input_code_tuple))
-                if qty[0][0] > 0:
-                    cursor.execute("UPDATE inventory SET qty = qty - 1 WHERE bar_code = ?", input_code_tuple)
-                    item = list(cursor.execute("SELECT * FROM inventory WHERE bar_code = ?", input_code_tuple))
-                    print(f"Item {item[0][0]} {item[0][1]} quantity is now {item[0][2]}")
                 else:
-                    item = list(cursor.execute("SELECT * FROM inventory WHERE bar_code = ?", input_code_tuple))
-                    print(f"Item {item[0][0]} {item[0][1]} quantity is already 0.")
-            else:
-                print(f"Item {input_code_tuple[0]} not in inventory.")
+                    product_name = input(
+                        "Please enter the name of the product... "
+                    ).lower().strip().replace(' ', '_')
 
-            connection.commit()
+                    new_product = Inventory(
+                        barcode=input_barcode,
+                        product=product_name,
+                        quantity=1
+                    )
+                    session.add(new_product)
+
+                session.commit()
+
+    #
+    # elif task_code == "multiple-out":
+    #     print("'Multiple out' selected.")
+    #
+    #     while True:
+    #         input_code_tuple = input("Please scan a product or the 'done' code... "),
+    #         if input_code_tuple[0] == "done":
+    #             break
+    #
+    #         product_row = list(cursor.execute("SELECT bar_code FROM inventory WHERE bar_code = ?", input_code_tuple))
+    #
+    #         if input_code_tuple in product_row:
+    #             qty = list(cursor.execute("SELECT qty FROM inventory WHERE bar_code = ?", input_code_tuple))
+    #             if qty[0][0] > 0:
+    #                 cursor.execute("UPDATE inventory SET qty = qty - 1 WHERE bar_code = ?", input_code_tuple)
+    #                 item = list(cursor.execute("SELECT * FROM inventory WHERE bar_code = ?", input_code_tuple))
+    #                 print(f"Item {item[0][0]} {item[0][1]} quantity is now {item[0][2]}")
+    #             else:
+    #                 item = list(cursor.execute("SELECT * FROM inventory WHERE bar_code = ?", input_code_tuple))
+    #                 print(f"Item {item[0][0]} {item[0][1]} quantity is already 0.")
+    #         else:
+    #             print(f"Item {input_code_tuple[0]} not in inventory.")
+    #
+    #         connection.commit()
 
     elif task_code == "exit":
         break
