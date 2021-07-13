d475321044c6c354ba2ab994bc9787da57746b58
Mathieu <math_morin@msn.com>
2021-07-13 13:12:01 -0400

    Refactoring of the Multiple-out proto-logic with ORM

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy import Column
 from sqlalchemy import Integer, String
-from sqlalchemy import insert, select
+from sqlalchemy import select
 from sqlalchemy.orm import Session, declarative_base
 
 
@@ -17,11 +17,13 @@ class Inventory(Base):
     __tablename__ = 'inventory'
 
     barcode = Column(String, primary_key=True)
-    product = Column(String, nullable=False)
+    name = Column(String, nullable=False)
     quantity = Column(Integer, nullable=False)
 
     def __repr__(self):
-        return f"Inventory(bar_code={self.barcode!r}, product={self.product!r}, quantity={self.quantity!r})"
+        return f"Inventory(bar_code={self.barcode!r}, " \
+               f"name={self.name!r}, " \
+               f"quantity={self.quantity!r})"
 
 
 Base.metadata.create_all(engine)
@@ -37,7 +39,7 @@ while True:
 
         while True:
             input_barcode = input(
-                "Please scan a product or the 'done' barcode... "
+                "Please scan an item or the 'done' barcode... "
             )
 
             if input_barcode == "done":
@@ -49,51 +51,76 @@ while True:
                 ).scalars()
 
                 if input_barcode in barcodes_in_inventory:
-                    product_to_update = session.execute(
+                    item_to_update = session.execute(
                         select(Inventory).
                         filter_by(barcode=input_barcode)
                     ).scalar_one()
-                    product_to_update.quantity += 1
-
+                    item_to_update.quantity += 1
 
                 else:
-                    product_name = input(
-                        "Please enter the name of the product... "
+                    item_name = input(
+                        "Please enter the name of the item... "
                     ).lower().strip().replace(' ', '_')
 
-                    new_product = Inventory(
+                    new_item = Inventory(
                         barcode=input_barcode,
-                        product=product_name,
+                        name=item_name,
                         quantity=1
                     )
-                    session.add(new_product)
+                    session.add(new_item)
 
                 session.commit()
 
-    #
-    # elif task_code == "multiple-out":
-    #     print("'Multiple out' selected.")
-    #
-    #     while True:
-    #         input_code_tuple = input("Please scan a product or the 'done' code... "),
-    #         if input_code_tuple[0] == "done":
-    #             break
-    #
-    #         product_row = list(cursor.execute("SELECT bar_code FROM inventory WHERE bar_code = ?", input_code_tuple))
-    #
-    #         if input_code_tuple in product_row:
-    #             qty = list(cursor.execute("SELECT qty FROM inventory WHERE bar_code = ?", input_code_tuple))
-    #             if qty[0][0] > 0:
-    #                 cursor.execute("UPDATE inventory SET qty = qty - 1 WHERE bar_code = ?", input_code_tuple)
-    #                 item = list(cursor.execute("SELECT * FROM inventory WHERE bar_code = ?", input_code_tuple))
-    #                 print(f"Item {item[0][0]} {item[0][1]} quantity is now {item[0][2]}")
-    #             else:
-    #                 item = list(cursor.execute("SELECT * FROM inventory WHERE bar_code = ?", input_code_tuple))
-    #                 print(f"Item {item[0][0]} {item[0][1]} quantity is already 0.")
-    #         else:
-    #             print(f"Item {input_code_tuple[0]} not in inventory.")
-    #
-    #         connection.commit()
+                updated_item = session.execute(
+                    select(Inventory).
+                    filter_by(barcode=input_barcode)
+                ).scalar_one()
+                print(f"Item {updated_item.barcode} '{updated_item.name}' "
+                      f"quantity is now {updated_item.quantity}.")
+
+    elif task_code == "multiple-out":
+        print("'multiple out' selected.")
+
+        while True:
+            input_barcode = input(
+                "Please scan an item or the 'done' barcode... "
+            )
+
+            if input_barcode == "done":
+                break
+
+            with Session(engine) as session:
+                barcodes_in_inventory = session.execute(
+                    select(Inventory.barcode)
+                ).scalars()
+
+                if input_barcode in barcodes_in_inventory:
+                    item_to_update = session.execute(
+                        select(Inventory).
+                        filter_by(barcode=input_barcode)
+                    ).scalar_one()
+
+                    if item_to_update.quantity > 0:
+                        item_to_update.quantity -= 1
+
+                        session.commit()
+
+                        updated_item = session.execute(
+                            select(Inventory).
+                            filter_by(barcode=input_barcode)
+                        ).scalar_one()
+
+                        print(f"Item {updated_item.barcode} "
+                              f"'{updated_item.name}' "
+                              f"quantity is now {updated_item.quantity}.")
+                    else:
+                        print(f"ERROR: Quantity can't ben decremented.")
+                        print(f"Item {item_to_update.barcode} "
+                              f"'{item_to_update.name}' "
+                              f"quantity is {item_to_update.quantity}.")
+
+                else:
+                    print(f"Item {input_barcode} not in inventory.")
 
     elif task_code == "exit":
         break
